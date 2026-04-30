441bba005823060be535f31a969c6db67d36c27d
Akhil <akhilpathiyamparambil21@gmail.com>
2026-04-30 18:34:13 +0530

    weekone final commit

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,17 +1,42 @@
-import categoryModel from "../model/categoryModel.js";
+// import categoryModel from "../model/categoryModel.js";
 
-// 
 
-export const adminCategoryLoad = async (req, res) => {
-    try {console.log("hit");
+
+// export const adminCategoryLoad = async (req,res)=>{
+
+//     let filter = {}  
+//     let sortOption = { createdAt: -1 };  
+
+//     if(req.query.status == "true"){
+//         filter.isActive = true
+//     }else if(req.query.status == "false"){
+//         filter.isActive = false
+//     }
+//     if (req.query.search&&req.query.search.trim()!=="") {
+//         filter.$or = [
+//             { categoryName: { $regex: req.query.search, $options: "i" } }
+            
+//         ];
+//     }
+
+//     let status = String(filter.isActive)
     
-        if (req.session.admin) {
-            return res.redirect("/admin/dashboard");
-        }
+//     if (req.query.sort === "oldest") {
+//         sortOption = { createdAt: 1 };
+//     }
+//     let data = await categoryModelLoad(filter,sortOption,req.query.page)
+//     let offer = await offerDataLoad()
+//     return res.render('Admin/categories-page',{
 
-        return res.render("Admin/categoryPage"); 
-    } catch (error) {
-        console.error("Error loading admin login:", error);
-        return res.status(500).send("Server Error");
-    }
-};
+//         data:data.data,
+//         error:'',
+//         status:status,
+//         search:req.query.search || "",
+//         sort: req.query.sort || "latest",
+//         currentPage:data.currentPage,
+//         totalUser:data.totalUser,
+//         totalPage:data.totalPages,
+//         activePage:'category',
+//         offer:offer.data
+//     })
+// }
